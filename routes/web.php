2c7edb45e9dcd20cfb1d694d12f17f277b87294a
Ömer Faruk KESMEZ <31587367+ofke-yazilim@users.noreply.github.com>
2017-12-09 10:41:54 +0200

    Route kullanım örnekleri yapıldı. @ofke Route üzerinden direk strin nasıl basılır gösterildi. Route üzerinde sql işlemleri yapıldı. Route üzerinde namespace kullanımörenği gösterildi. Route üzerinde resource kullanımı gösterildi. Route üzerinde group kullanımı gösterildi.

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
+//Route sayfaından controller çağırmadan direk view sayfasına ulaşmak
 Route::get('/', function () {
     return view('welcome');
 });
@@ -47,9 +48,7 @@ Route::get('/isimatama',["as"=>"routeisim",function(){
 //Route kullanarak controller dosyasına yönlendirme yapalım
 //Aşağıda yonlendircontroller adında route oluşturduk
 //Oluşan bu route newcontroller adındaki controller dosyamızdaki index methodunu çağırıyor.
-Route::get('/yonlendircontroller',["as"=>"yonlendircontroller" , "uses"=>"newcontroller@index",function(){
-
-}]);
+Route::get('/yonlendircontroller',["as"=>"yonlendircontroller" , "uses"=>"newcontroller@index",function(){}]);
 
 //Route üzerinde namspace kullanımı
 //Controllerin altında oluşturulan test1 klasörü içerisindeki controller dosyalarını çalıştırmamızı sağlar
@@ -58,6 +57,69 @@ Route::group(['namespace' => 'test1'],function (){
     //Parametre olarak id değeri alıyor id değeri buş giderse 12 değerrini alacak şekilde ayarlandı.
     Route::get('/test1/liste/{id?}',["as"=>"test1.liste","uses"=>"testController@index",function($id=12){}]);
 });
+
+############################## SQL İŞLEMLERİ ###########################################
+
+//Laravel Sql işlemleri için : https://laravel.com/docs/5.5/queries adresini inceleyin
+//aşağıda daha önce migration kullanarak oluşturmuş olduğum user tablosuna veri ekliyorum
+Route::get('/deneme/insert',  function(){
+    //User tablosuna örnek bir insert işlemi yapılması
+    $result = DB::table('user')->insert(
+        ['name' => 'Ömer Faruk' , 'surname' => 'KESMEZ' ,'email' => 'ofke@gmail.com', 'status' => 1 , 'created_at' => date("Y-m-d H:i:s") , 'sehir' => 'BURSA']
+    );
+    
+    //User tablosuna 2 adet insert yapılıyor
+    $result = DB::table('user')->insert([
+        ['name' => 'Halil İbrahim' , 'surname' => 'YURT' ,'email' => 'halil@gmail.com', 'status' => 1 ,  'age' => 25 , 'created_at' => date("Y-m-d H:i:s") , 'sehir' => 'FETHİYE'],
+        ['name' => 'YUNUS' , 'surname' => 'FARDA' ,'email' => 'yunus@gmail.com', 'status' => 1 , 'age' => 20 , 'created_at' => date("Y-m-d H:i:s") , 'sehir' => 'BURSA']   
+    ]);
+    
+    //dd methodu var_dump() özelliği taşır sonuçları almamızı sağlar
+    return dd($result);
+});
+
+//Aşağıda user tablomuzdan select işlemi ile veri çekme işlemi yapılıyor
+Route::get('deneme/select/{id?}',function($id=null){
+    //Basit bir select işlemi user tablosundan "name,surname,email" verilerini getiriyor.
+    $result = DB::table('user')->select('name','surname','email')->get();
+    
+    //where kullanımlı select sorgusu status değer 1 olan kişilerin isimlerini getirir.
+    $result = DB::table('user')->select('name')->where('status','=',1)->get();
+    
+    if($id){
+        //2 where , 1 orderby kullanımlı select sorgusu status değer 1 olan kişilerin isimlerini ve id değerlerini getirir.
+        $result = DB::table('user')->select('id','name')->where('status','=',1)->where('id',"=",$id)->orderBy('id','desc')->get();
+    } else{
+        //1 where ,orderby kullanımlı select sorgusu status değer 1 olan kişilerin isimlerini ve id değerlerini getirir.
+        $result = DB::table('user')->select('id','name')->where('status','=',1)->orderBy('id','desc')->get();
+    }
+    
+    //Select sonuçları ekrana baslıyor
+    return dd($result);
+    
+    //Aşağıdaki şekildede listeleyebilirdik.
+    //return $result;
+});
+
+//id değeri gönderilen kullanıcı veritabanı üzerinden silinir.
+Route::get('deneme/delete/{id}',function($id){
+    //id değeri belirtilen kullanıcıyı siler
+    $result = DB::table('user')->where('id','=',$id)->delete();
+    
+    //Silme başarılı ise 1 başarısız ise  0 döner
+    return $result;
+});
+
+//Update işlemleri id değeri gönderlen kullanıcının verileri güncellenir
+Route::get('deneme/update/{id}',function($id){
+    //id değeri gönderilen kullanıcının name,surname,status alanları güncellenir.
+    $result = DB::table('user')->where('id',"=",$id)->update(['name'=>'Halil İbrahim','surname'=>'KESMEZ','status'=>1]);
+    
+    //Güncelleme başarılı ise 1 başarısız ise  0 döner
+    return $result;
+});
+
+############################ SQL ÖRNEKLERİ SON ##################################
 
 //Controller/admin/user.php controller dosyasına ulaşmak için
 Route::group(["namespace"=>"admin"],function(){
