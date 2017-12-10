2779bf6d65b68d1694d18c8ee555656dce5fd559
Ömer Faruk KESMEZ <31587367+ofke-yazilim@users.noreply.github.com>
2017-12-10 13:18:08 +0200

    Kulanici adında bir model oluşturdum.@ofke Oluşturmuş olduğum bu dodel dosyamın database üzerinde kullanacağı tablo isimi belirttim.

---
+++ app/kullanici.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class kullanici extends Model
+{
+    //Kullanacağım tablo ismini belirtiyorum.
+    protected $table = "kullanicilar";
+}
