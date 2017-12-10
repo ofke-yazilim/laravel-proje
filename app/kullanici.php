c301ce2e7034ab0d345c20e3b7a12a6f155da2dc
Ömer Faruk KESMEZ <31587367+ofke-yazilim@users.noreply.github.com>
2017-12-10 22:56:00 +0200

    Kullanici modelimiz üzerinde massAssignment@ofke Model dosyamız üzerinde massAssignment tannımlaması yapmayı ve işlem yapacağımız database içerisindeki tablo adımızı tanımlamayı gösterdim.

--- app/kullanici.php
+++ app/kullanici.php
@@ -8,4 +8,18 @@ class kullanici extends Model
 {
     //Kullanacağım tablo ismini belirtiyorum.
     protected $table = "kullanicilar";
+    
+    //Önemli : $fillable ve $guarted dizileri aynı anda tanımlanamaz.
+    //ORM üzerinde ::create,::update,::delete  methodları ile işlem yapmak için iki tanımdan birinin kullanımı şarttır.
+    //Bu tanımlama ile bu model yapısı ile veri tabanına kayıt yaparken sadece bu sütun isimlerini istediğimizi belirtiyoruz.
+    protected $fillable = [
+        'name',
+        'surname',
+        'status'
+    ];
+    
+    //fillablenin tam tersi olark çalışır sehir hariç sütunları kayıt eder.
+//    protected $guarded = [
+//        'sehir'
+//    ];
 }
