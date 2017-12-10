f9ea1e2b6ea5a30b4e56a8fcf23d7645858a11ee
Ömer Faruk KESMEZ <31587367+ofke-yazilim@users.noreply.github.com>
2017-12-10 13:17:06 +0200

    Controller dosyam üzerinde ORM kullanımörnekleri gerçekleştirdim.@ofke Daha önce Route dosyam üzerinde kullanmış olduğum query builder methodlarının ORM üzeriinde nasıl gereçeklenebileceğini inceledim.

--- app/Http/Controllers/newcontroller.php
+++ app/Http/Controllers/newcontroller.php
@@ -3,12 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+//İşlem yapacağımız model dosyamız tanıtıldı
+use App\kullanici;
 
 class newcontroller extends Controller
 {
     //index methodu
+    /*
+     * Bu method içerisinde ORM kullanarak sql işlemleri gerçekleştiriyorum.
+     */
     public function index(){
-        return "Controller içerisine yöneldik.";
+        /*
+         * query builder içerisinde kullandığım tüm methodlar ORM içinde geçerlidir.
+         */
+        //ORM Kullanarak insert işlemi Örneği
+        $kullanici = new kullanici;
+        $kullanici->name = "Ömer Faruk";
+        $kullanici->surname = "KESMEZ";
+        $kullanici->email = "info@ofkeyazilim.com";
+        $kullanici->save();
+
+        //ORM Kullanarak Update İşlemi Örneği
+        $kullanici = kullanici::find(1);
+        $kullanici->name = "Ömer Faruk";
+        $kullanici->surname = "KESMEZ";
+        $kullanici->email = "ofke@ofkeyazilim.com";
+        $kullanici->save();
+        
+        //id değeri 1 olan kullanıcı verilerini döndürür
+        $result = kullanici::find(1);
+        
+        //kullanici tablosundan id değeri 2 den büyük olanların içinde id değeri en büyük olan 1 değer döndürecek.
+        $result = kullanici::where("id",">",2)->orderBy('id','desc')->first();
+        
+        //Sonuçlar döndürülüyor
+        return dd($result);
     }
     
     //Collection işlemleri
@@ -20,7 +49,7 @@ class newcontroller extends Controller
         #### TEK BOYUTLU COLLECTION #####
         #
         //Array bir dizi oluşturuluyor.
-        $data = [1,2,3,4,5,6,7,8,9];
+        $data = [1,2,3,4,5,6,7,8,9,"ömer",""];
        
         //Oluşturulan dizi collection olarak set ediliyor
         $collection = collect($data);
@@ -53,8 +82,17 @@ class newcontroller extends Controller
         });
         $collection->all();
         
+        //Aşağıdaki kod collection içerisindeki tüm dataları istenilen şekilde işler
+        //Bütün değerleri büyük harf yapar. ve boş olan değerleri collection içerisinden çıkarır
+        //Transform aksine yeni bir collection oluşturur
+        $collection = $collection->map(function($item) {
+            return strtoupper($item);
+        })
+        ->reject(function ($item) {
+            return empty($item);
+        });
         
-        //Her bir collection üzerinde işlem yapmaızı ve güüncelleme yapmamızı sağlar.
+        //Her bir collection üzerinde işlem yapmaızı ve güncelleme yapmamızı sağlar.
         $collection->transform(function ($item) {
             return $item * 2;
         });
@@ -73,5 +111,51 @@ class newcontroller extends Controller
         
         //Oluşturduğumuz bu collectionu wiew dosyamıza gönderelim
 //        return view('collection', compact('collection'));   
+    }
+    
+    /*
+     * 2 boyutlu array ile yapılan collection işlemleri
+     */
+    public function collection2() {
+        //2 boyutlu dizimiz tanmlandı  
+        $array = ["ad"=>"Ömer","ad"=>"Yasin","ad2"=>"Faruk","soyad"=>"KESMEZ","il"=>"BURSA"];
+        
+        //2 boyutlu Collection oluşturuluyor
+        $collection = collect($array);
+        
+        //oluşturulan collection ekrana yazılıyor // view içerisinde kullanılıyor olabilir.
+        $collection->all();
+        
+        //Collection değerleri içersinde arama yapar
+        $result = $collection->contains('Faruk');//true döner
+        
+        //Üç boyutlu bir array içerisinde arama yapmak için
+        //Üç boyutlu bir collection
+        $collection3 = collect([
+            ['product' => 'Desk', 'price' => 200],
+            ['product' => 'Chair', 'price' => 100,"il"=>"BURSA"],
+            ['product' => 'Chair', 'price' => 100,"il"=>"BURSA"],
+        ]);
+
+        $result = $collection3->contains('price', 200);
+        
+        //Collection eleman sayısı döner 
+        $result = $collection3->count();
+        
+        //2 buyutlu collection içerisinde belirtilen değerler çıkarır
+        $result = $collection->diffAssoc([
+            'il' => "BURSA"
+        ]);
+        
+        //2 buyutlu collection içerisinde belirtilen anahtara sahip değeri siler
+        $result = $collection->diffKeys([
+            'il' => "ANGARA"
+        ]);
+        
+        //3 boyutlu collection içerisinde verilen anahtara uygun olarak gruplar
+        $result = $collection3->groupBy('product');
+        $result->toArray();
+        
+        return dd($result);
     }
 }
