0789223170fe2c1ee85518e6e3f3b1d6e5559313
Ömer Faruk KESMEZ <31587367+ofke-yazilim@users.noreply.github.com>
2017-12-11 08:57:40 +0200

    images modeli ile one to one relationships.@ofke kullanici tablosu ile images tablosu arasında one to one relationships türünde bağlantı oluşturuldu.

--- app/Image.php
+++ app/Image.php
@@ -18,4 +18,12 @@ class Image extends Model
         'updated_at'
     ];
     
+    //Kullanıcı tablosu ile one to one relationships bağlantısı
+    public function kullanici() {
+        //App\kullanici => ilişki kuracağımız modeli temsil eder.
+        //id=> kullanici tablosunda bulunan ve kullanici id değerini taşyan sütundur.
+        //kullanici_id=> images tablosunda bulunan ve kullanici id değerini taşyan sütundur.
+        $this->hasOne("App\kullanici", "id", "kullanici_id");
+    }
+    
 }
