c1dcef9190d21f22166f3885a9397184e4c3dfa6
ofke <ofke_erz@hotmail.com>
2017-12-12 17:41:55 +0200

    urunler tablosu için oluşturulmuş model dosyası.@ofke Orm işlmlerinde urunler tablosu ile yapılacak işlemleri bu model dosyasıo ile yönetebileceğim.

---
+++ app/Urun.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Urun extends Model
+{
+    //Urun modelimizde kullanacağımız tablo adı tanımlandı.
+    protected $table = "urunler";
+    
+    //Many to Many Relationships
+    //Kullanici modeli ile bağlantı tablosu aracılığı ile ilşiki kuruluyor
+    protected function kullanicilar() {
+        return $this->belongsToMany("App\kullanici", "urun_kullanici_baglanti", "urun_id");
+    }
+}
