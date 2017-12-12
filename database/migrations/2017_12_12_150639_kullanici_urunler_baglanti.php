8abba204e912e4fafa137b06ba5578caac7515ff
ofke <ofke_erz@hotmail.com>
2017-12-12 17:40:46 +0200

    Urun ve Urun Kullanıcı bağlatı tablosu migration.@ofke urunler tablosu ve urunler yablosu ile kulllanici tablosu arasına Many To Many Relatioships bağlantsını sağlayan tablaları oluşturmak için hazırlanmış migration dosyalrıdır.

---
+++ database/migrations/2017_12_12_150639_kullanici_urunler_baglanti.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class KullaniciUrunlerBaglanti extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('urun_kullanici_baglanti', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('kullanici_id');
+            $table->integer('urun_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('urun_kullanici_baglanti');
+    }
+}
