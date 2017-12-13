42f4b8f1060d7b0edcb976c92e5711fb322dfe32
Ömer Faruk KESMEZ <31587367+ofke-yazilim@users.noreply.github.com>
2017-12-13 08:53:54 +0200

    Many to Many Pivot Kullanımı yapıldı.@ofke Many To Many Relatiıonships fonksiyonu içerisinde pivot örneği yapıldı bu sayede bağlantı tablosundan stenilen sütunların alınması sağlanabiliyor.

--- app/Urun.php
+++ app/Urun.php
@@ -12,6 +12,7 @@ class Urun extends Model
     //Many to Many Relationships
     //Kullanici modeli ile bağlantı tablosu aracılığı ile ilşiki kuruluyor
     protected function kullanicilar() {
-        return $this->belongsToMany("App\kullanici", "urun_kullanici_baglanti", "urun_id");
+        return $this->belongsToMany("App\kullanici", "urun_kullanici_baglanti", "urun_id")
+            ->withTimestamps()->withPivot('kargo');
     }
 }
