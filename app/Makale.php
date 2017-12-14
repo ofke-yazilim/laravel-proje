416a7d6ce3d58da9e5c86240b7f82586df06b0bd
ofke <ofke_erz@hotmail.com>
2017-12-14 10:20:16 +0200

    One To Many Polimorfik Relatioships Örneği.@ofke

--- app/Makale.php
+++ app/Makale.php
@@ -20,4 +20,12 @@ class Makale extends Model
     protected function kullanciResimler() {
         return $this->hasManyThrough("App\Image", "App\kullanici", "image_id","kullanici_id", "id");
     }
+    
+    //Polimorphic One To Many Relatioships 
+    //Makale ve Ürün resimlerinin turulduğu resimler tablosundan makale resimleri alınıyor.
+    protected function MakaleResimler() {
+        //Hatırlarsanız resimler tablosuna  bağlantı id değerimi ve bağlatı typ değerlerimi "image" başlığını kullanarak gerçekledim.
+        return $this->morphMany("App\Resim", "image");
+    }
+    
 }
