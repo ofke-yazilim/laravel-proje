0305ee6f7c569de84c96994fd3c680f5fe78a473
ofke <ofke_erz@hotmail.com>
2017-12-14 10:20:38 +0200

    Düzenleme yapıldı.@ofke

--- app/Urun.php
+++ app/Urun.php
@@ -13,6 +13,7 @@ class Urun extends Model
     //Kullanici modeli ile bağlantı tablosu aracılığı ile ilşiki kuruluyor
     protected function kullanicilar() {
         return $this->belongsToMany("App\kullanici", "urun_kullanici_baglanti", "urun_id")
-            ->withTimestamps()->withPivot('kargo');
+            ->withTimestamps()->withPivot('kargo')
+                ;
     }
 }
